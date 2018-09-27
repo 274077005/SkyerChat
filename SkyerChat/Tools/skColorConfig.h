e315ee4b970fbfc952d6231864ba0140bcbb14bb
skyer <274077005@qq.com>
2018-09-27 15:10:07 +0800

    添加颜色宏
    添加颜色宏

---
+++ SkyerChat/Tools/skColorConfig.h
@@ -0,0 +1,46 @@
+//
+//  skCorlorConfig.h
+//  SkyerChat
+//
+//  Created by admin on 2018/9/27.
+//  Copyright © 2018年 www.skyer.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface skColorConfig : NSObject
+
+/**
+ 主色调
+ */
+#define colorMain skUIColorFromRGB(0x11ccff)
+/**
+ 辅助色调红
+ */
+#define colorRed skUIColorFromRGB(0xff4455)
+/**
+ 辅助色调绿
+ */
+#define colorGreen skUIColorFromRGB(0x22aa33)
+/**
+ 辅助色调粉
+ */
+#define colorPink skUIColorFromRGB(0xffaa11)
+/**
+ 字体颜色黑
+ */
+#define colorTextBlack skUIColorFromRGB(0x000000)
+/**
+ 字体颜色浅
+ */
+#define colorTextLight skUIColorFromRGB(0x999999)
+/**
+ 背景颜色
+ */
+#define colorBackground skUIColorFromRGB(0xf5f5f5)
+/**
+ 线条颜色
+ */
+#define colorLine skUIColorFromRGB(0xd8d8d8)
+
+@end
---
+++ SkyerChat/Tools/skColorConfig.m
@@ -0,0 +1,16 @@
+//
+//  skCorlorConfig.m
+//  SkyerChat
+//
+//  Created by admin on 2018/9/27.
+//  Copyright © 2018年 www.skyer.com. All rights reserved.
+//
+
+#import "skColorConfig.h"
+
+@implementation skColorConfig
+-(void)test{
+//    [UIColor];
+    
+}
+@end
