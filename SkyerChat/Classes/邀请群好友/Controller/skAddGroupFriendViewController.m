c928c0f68dd37ddfe78c9aa805f4758bc8e60bc9
skyer <274077005@qq.com>
2018-11-22 10:35:45 +0800

    11.22
    11.22

--- SkyerChat/Classes/邀请群好友/Controller/skAddGroupFriendViewController.m
+++ SkyerChat/Classes/邀请群好友/Controller/skAddGroupFriendViewController.m
@@ -8,14 +8,26 @@
 
 #import "skAddGroupFriendViewController.h"
 #import "skLookoverTableViewCell.h"
+#import "groupUserModel.h"
+#import "myClientModel.h"
+#import "UIImageView+WebCache.h"
 
 @interface skAddGroupFriendViewController ()
-@property (nonatomic,strong) NSArray *arrList;
+@property (nonatomic,strong) NSArray *arrListGroupMenber;
+@property (nonatomic,strong) NSArray *arrListMyFriend;
 @property (nonatomic,strong) NSMutableArray *arrSelect;
 @property (nonatomic,strong) UIButton *btnRight;
+@property (nonatomic,strong) NSMutableArray *arrCanAddMember;
 @end
 
 @implementation skAddGroupFriendViewController
+
+-(NSMutableArray *)arrCanAddMember{
+    if (nil==_arrCanAddMember) {
+        _arrCanAddMember=[[NSMutableArray alloc] init];
+    }
+    return _arrCanAddMember;
+}
 - (NSMutableArray *)arrSelect{
     if (nil==_arrSelect) {
         _arrSelect=[[NSMutableArray alloc] init];
@@ -29,7 +41,7 @@
         [[_btnRight rac_signalForControlEvents:(UIControlEventTouchUpInside)] subscribeNext:^(__kindof UIControl * _Nullable x) {
             @strongify(self)
             if (self.arrSelect.count>0) {
-                
+                [self bizGroupUserCreate];
             }
         }];
     }
@@ -41,6 +53,8 @@
     // Do any additional setup after loading the view.
     [self addTableView];
     [self.btnRight setTitle:@"确定(0)" forState:(UIControlStateNormal)];
+    [self bizGroupUserlist];
+    [self getAddressBookList:@""];
 }
 -(void)addTableView{
     [self.view addSubview:self.tableView];
@@ -63,7 +77,7 @@
 #pragma mark - 代理方法
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.arrList.count;
+    return self.arrCanAddMember.count;
 }
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     return 50;
@@ -80,6 +94,7 @@
     [cell setAccessoryType:(UITableViewCellAccessoryNone)];
     [cell.imageSelect setHidden:NO];
     [cell.labType setHidden:YES];
+    myClientModel *modelC=[self.arrCanAddMember objectAtIndex:indexPath.row];
     
     NSString *row=[NSString stringWithFormat:@"%ld",indexPath.row];
     if ([self.arrSelect containsObject:row]) {
@@ -87,6 +102,9 @@
     }else{
         [cell.imageSelect setImage:[UIImage imageNamed:@"多选-未选"]];
     }
+    [cell.imageHeader sd_setImageWithURL:[NSURL URLWithString:modelC.portrait] placeholderImage:[UIImage imageNamed:@"default_portrait_msg"]];
+    cell.labName.text=modelC.nickName.length>0?modelC.nickName:modelC.userNo;
+    
     return cell;
 }
 
@@ -101,4 +119,99 @@
     [self.btnRight setTitle:[NSString stringWithFormat:@"确定(%ld)",self.arrSelect.count] forState:(UIControlStateNormal)];
     [self.tableView reloadData];
 }
+
+#pragma mark 群成员列表
+
+-(void)bizGroupUserlist{
+    ///intf/bizUser/sendRegister
+    NSDictionary *dic=@{@"groupNo":self.modelOther.groupNo
+                        };
+    
+    
+    [skAfTool SKPOST:skUrl(@"/intf/bizGroupUser/list") pubParame:skPubParType(0) busParame:[dic skDicToJson:dic] showHUD:YES showErrMsg:YES success:^(skResponeModel *  _Nullable responseObject) {
+        
+        if (responseObject.returnCode==0) {
+            skResponeList *modelList=[skResponeList mj_objectWithKeyValues:responseObject.data];
+            
+            self.arrListGroupMenber=[groupUserModel mj_objectArrayWithKeyValuesArray:modelList.list];
+            [self skDataSelect];
+        }
+        
+    } failure:^(NSError * _Nullable error) {
+        
+    }];
+}
+/**
+ 获取通讯录列表
+ 
+ @param keyword 关键字
+ */
+-(void)getAddressBookList:(NSString *)keyword{
+    NSDictionary *dic=@{@"keywordss":keyword};
+    if (keyword.length>0) {
+        dic=@{@"keyword":keyword};
+    }
+    
+    
+    [skAfTool SKPOST:skUrl(@"/intf/bizLinker/list") pubParame:skPubParType(0) busParame:[dic skDicToJson:dic] showHUD:NO showErrMsg:NO success:^(skResponeModel *  _Nullable responseObject) {
+        
+        if (responseObject.returnCode==0) {
+            
+            skResponeList *modelList=[skResponeList mj_objectWithKeyValues:responseObject.data];
+            
+            self.arrListMyFriend= [myClientModel mj_objectArrayWithKeyValuesArray:modelList.list];
+            
+            [self skDataSelect];
+        }
+        
+    } failure:^(NSError * _Nullable error) {
+        
+    }];
+}
+
+
+-(void)skDataSelect{
+    
+    self.arrCanAddMember=[[NSMutableArray alloc] initWithArray:self.arrListMyFriend];
+    for (int i=0; i<self.arrListMyFriend.count; ++i) {
+        myClientModel *modelC=[self.arrListMyFriend objectAtIndex:i];
+        NSString *userNoC=modelC.userNo;
+        
+        for (int k=0; k<self.arrListGroupMenber.count; ++k) {
+            groupUserModel *modelG=[self.arrListGroupMenber objectAtIndex:k];
+            NSString *userNoG=modelG.userNo;
+            if ([userNoC isEqualToString:userNoG]) {
+                [self.arrCanAddMember removeObject:modelC];
+            }
+        }
+    }
+    [self.tableView reloadData];
+}
+
+-(void)bizGroupUserCreate{
+    ///intf/bizUser/sendRegister
+    NSMutableArray *arrNos=[[NSMutableArray alloc] init];
+    for (int i =0; i<self.arrCanAddMember.count; ++i) {
+        myClientModel *modelc=[self.arrCanAddMember objectAtIndex:i];
+        [arrNos addObject:modelc.userNo];
+    }
+    NSDictionary *dic=@{@"groupNo":self.modelOther.groupNo,
+                        @"userNos":arrNos
+                        };
+    
+    
+    [skAfTool SKPOST:skUrl(@"/intf/bizGroupUser/create") pubParame:skPubParType(0) busParame:[dic skDicToJson:dic] showHUD:YES showErrMsg:YES success:^(skResponeModel *  _Nullable responseObject) {
+        
+        if (responseObject.returnCode==0) {
+            [SkToast SkToastShow:@"加群成功" withHight:300];
+            [self bizGroupUserlist];
+            [self getAddressBookList:@""];
+        }
+        
+    } failure:^(NSError * _Nullable error) {
+        
+    }];
+    
+}
+
 @end
